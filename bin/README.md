b534efa7db7ab7b18fc0ec298e0aa936b03a187f
Marco Nicosia <mnicosia@pivotal.io>
2016-12-12 16:50:13 -0800

    Add load-data.pl utility script

---
+++ bin/README.md
@@ -0,0 +1,26 @@
+I thought I'd be clever, deploy this all onto a c4.2xlarge and write all the data loading in a single perl script, so as to avoid lots of forking of the mysql client. That was harder than I'd like, here are some notes:
+
+1. Installing `DBD::mysql` is a pain on a bosh stemcell. Among others, you need to specify where to find the mysqld_config:
+  - Become root.
+    - ```cpan -fi DBD::DBI```
+    - Attempt to install DBD::mysql which will fail:```cpan -fi DBD::mysql```
+    - Look for the DBD::mysql directory: ```find Makefile.PL```
+    - ```perl Makefile.PL --testsocket=/var/vcap/data/sys/run/mysql/mysqld.sock --testuser=root --testpassword=[REDACTED] --mysql_config=/var/vcap/data/packages/mariadb/659e865fbbfd497444cb2114adbe41890f62e1a9.1-c37afa67a1f526176097dce0315b78ffddeb7aed/bin/mysql_config```
+    - I didn't get the `mysqld.sock` to work. Hence `make` works, but `make test` does not. `make install` works anyways. (I think it fails because we disallow test database.)
+1. You can't use `/var/tmp` because there's not much space there. I've modified the script to use `/var/vcap/store/tmp` but you have to create that manually as root, then make it world writable.
+
+Create or clear out the accept database:
+> mysql -u root -p -e "drop database accept ; create database accept ; grant all privileges on accept.* to 'root'@'localhost'";
+  - I think that last is required to enable `LOAD DATA INFILE` although I haven't gotten `LOAD DATA LOCAL INFILE` to work without changing the `my.cnf`.
+
+I ran this in parallel using this syntax:
+```
+$ time DB_USER=root DB_PASSWORD=[REDACTED] nohup ~/load-data.pl 1 10000 > load-1.out 2>&1 &
+...
+$ time DB_USER=root DB_PASSWORD=[REDACTED] nohup ~/load-data.pl 60001 70000 > load-7.out 2>&1 &
+```
+
+You can monitor progress by:
+- `tail -f load-*.out`
+- `iostat -xm 1 4`
+- `top`
---
+++ bin/load-data.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl -w
+
+# Create a bunch of random data files, indexed by range, then load them into a MySQL DB
+#
+# Usage: load.data.pl rangeStart rangeEnd
+#
+# Requirements:
+#  Always assumes a pre-existing database called "accept", writable by the creds provided
+#  Always assumes a pre-existing directory "/var/vcap/store/tmp" ## FIXME: Configurable TMPDIR?
+#  Needs DB_USER and DB_PASSWORD environment variables
+
+$| = 1 ;
+use DBI ;
+
+sub writeRandomData {
+    my ($filename, $numRecords) = @_ ;
+    my ($i, $j, $q) ;
+
+    open(DATA,">",$filename) || die ;
+    for ($i = 0 ; $i < $numRecords ; $i++) {
+        $j = int(rand(10000)) ;
+        $q = int(rand(10000)) ;
+        print DATA "$j,$q\n" ;
+    }
+    close(DATA) ;
+
+    return 1 ;
+}
+
+sub createDataSet {
+    my $range = shift(@_) ;
+    my $a ;
+
+    for ($a = $range->{"start"} ; $a <= $range->{"end"} ; $a++) {
+        if (! int($a % 1000)) { print " ... writing datafiles $a" }
+        writeRandomData("/var/tmp/randomdata$a.csv", 10000) ;
+    }
+    print "\nwriting datafiles done: ", $a-1, "\n" ;
+}
+
+sub importDataSet {
+    my $range = shift(@_) ;
+    my $x ;
+    my $dbh = DBI->connect("DBI:mysql:database=accept;host=localhost", $dbUser, $dbPassword) ;
+    
+    for ($x = $range->{"start"} ; $x <= $range->{"end"} ; $x++) {
+        if (! int($x % 1000)) { print " ... loaded $x" }
+        $dbh->do("SET FOREIGN_KEY_CHECKS = 0") ;
+        $dbh->do("SET UNIQUE_CHECKS = 0") ;
+        $dbh->do("SET SESSION tx_isolation='READ-UNCOMMITTED'") ;
+        $dbh->do("SET sql_log_bin = 0") ;
+        $dbh->do("CREATE TABLE randomdata$x (col1 INT, col2 INT)") ;
+        $dbh->do("LOAD DATA LOCAL INFILE '/var/tmp/randomdata$x.csv' IGNORE INTO TABLE randomdata$x fields terminated by ','") ;
+    }
+    $dbh->disconnect() ;
+    print "\nloading done: ", $x-1, "\n" ;
+}
+
+### MAIN
+
+if (! -d "/var/vcap/store/tmp") {
+    die "[ERROR] missing required /var/vcap/store/tmp directory" ;
+}
+
+if (1 > $#ARGV) {
+    die "[ERROR] Missing command line arguments Start and End of range" ;
+} else {
+    %range = ( "start" => $ARGV[0], "end" => $ARGV[1] ) ;
+}
+
+&createDataSet(\%range) ;
+&importDataSet(\%range) ;
+
+exit 0 ;
