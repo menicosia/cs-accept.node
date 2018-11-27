aace811fe3d1c3d10f005b002a53b97add5485bb
Marco Nicosia <mnicosia@pivotal.io>
2018-11-26 17:50:07 -0800

    Guard logic if not bound to any service

--- bind-mysql.js
+++ bind-mysql.js
@@ -1,7 +1,8 @@
 module.exports.getMySQLCreds = function(service) {
 
     var mysql_creds = {} ;
-    if (process.env.VCAP_SERVICES) {
+    var service = undefined ; 
+    if (process.env.VCAP_SERVICES && Object.keys(process.env.VCAP_SERVICES).length) {
         var vcap_services = JSON.parse(process.env.VCAP_SERVICES) ;
         if (vcap_services['p.mysql']) {
             service = "p.mysql" ;
@@ -12,17 +13,20 @@ module.exports.getMySQLCreds = function(service) {
         } else if (vcap_services['pxc']) {
             service = "pxc" ;
         }
-        mysql_creds["host"] = vcap_services[service][0]["credentials"]["hostname"] ;
-        mysql_creds["user"] = vcap_services[service][0]["credentials"]["username"] ;
-        mysql_creds["password"] = vcap_services[service][0]["credentials"]["password"] ;
-        mysql_creds["port"] = vcap_services[service][0]["credentials"]["port"] ;
-        mysql_creds["database"] = vcap_services[service][0]["credentials"]["name"] ;
-        if (vcap_services[service][0]["credentials"]["tls"]) {
-            mysql_creds["ca_certificate"] = vcap_services[service][0]["credentials"]["tls"]["cert"]["ca"];
-        } else {
-            mysql_creds["ca_certificate"] = undefined ;
+        if (service) {
+            console.log("Discovering credentials for: " + service) ;
+            mysql_creds["host"] = vcap_services[service][0]["credentials"]["hostname"] ;
+            mysql_creds["user"] = vcap_services[service][0]["credentials"]["username"] ;
+            mysql_creds["password"] = vcap_services[service][0]["credentials"]["password"] ;
+            mysql_creds["port"] = vcap_services[service][0]["credentials"]["port"] ;
+            mysql_creds["database"] = vcap_services[service][0]["credentials"]["name"] ;
+            if (vcap_services[service][0]["credentials"]["tls"]) {
+                mysql_creds["ca_certificate"] = vcap_services[service][0]["credentials"]["tls"]["cert"]["ca"];
+            } else {
+                mysql_creds["ca_certificate"] = undefined ;
+            }
+            mysql_creds["uri"] = vcap_services[service][0]["credentials"]["uri"] ;
         }
-        mysql_creds["uri"] = vcap_services[service][0]["credentials"]["uri"] ;
     } else {
         console.log("No VCAP_SERVICES in environment; using localhost") ;
         service = "local" ;
